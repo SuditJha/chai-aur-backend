118f0d223e55d98d4d27c28e74d30de90e449f69
Sudit Kumar Jha <jsudit2@gmail.com>
2024-04-27 17:07:53 +0530

    Added User Model

---
+++ src/models/user.model.js
@@ -0,0 +1,52 @@
+import mongoose, { Schema } from "mongoose"
+
+const userSchema = Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+        fullName: {
+            type: String,
+            required: true,
+            trim: true,
+            index: true,
+        },
+        avtar: {
+            type: String, // cloudinary
+            required: true,
+        },
+        avtar: {
+            type: String, // cloudinary
+        },
+        watchHistory: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Video"
+            }
+        ],
+        password: {
+            type: String,
+            required: [true, "Password is required"]
+        },
+        refreshToken: {
+            type: String
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+export const user = mongoose.model("User", userSchema)
