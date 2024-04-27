7642e37a72e8e3e55f4f06079d95e95f601ca409
Sudit Kumar Jha <jsudit2@gmail.com>
2024-04-27 18:45:52 +0530

    Used bcrypt to encrypt/dcrypt password, added custom metods to check if password is correct Used JWT package to generate ACCESS/REFRESH tokens which will be used in the future

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,9 @@
 import mongoose, { Schema } from "mongoose"
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2"
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 
-const userSchema = Schema(
+const userSchema = new Schema(
     {
         username: {
             type: String,
@@ -48,5 +51,49 @@ const userSchema = Schema(
         timestamps: true
     }
 )
+// Encrypting the password 
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next()
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
 
-export const user = mongoose.model("User", userSchema)
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = async function () {
+    return await jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = async function () {
+    return await jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+
+
+userSchema.methods.generateRefreshToken = async function () { }
+
+export const User = mongoose.model("User", userSchema)
