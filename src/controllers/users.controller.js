6ccfee1a8a8ba451b128ec3ca3a06e68620c2ee2
Sudit Kumar Jha <jsudit2@gmail.com>
2024-05-20 18:34:39 +0530

    Logout controller completed

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -150,10 +150,39 @@ const loginUser = asyncHandler(async (req, res) => {
             )
         )
 
+
+
+})
+
+const logoutUser = asyncHandler(async (req, res) => {
+    const user = await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken: undefined
+            }
+        },
+        {
+            new: true
+        }
+    )
+    const cookieOptions = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res.status(200)
+        .clearCookie("accessToken", cookieOptions)
+        .clearCookie("refreshToken", cookieOptions)
+        .json(
+            new ApiResponse(200, {}, "User successfully logged out")
+        )
+
 })
 
 export {
     registerUser,
-    loginUser
+    loginUser,
+    logoutUser
 }
 
